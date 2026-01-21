49a659e51de7f973b40a00dbf4f511d88aef962e
zhizhi051207 <zhizhi051207@gmail.com>
2026-01-21 13:36:01 +0800

    Delete camera changing directory

--- camera changing/backend/app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.endpoints import images, users, health
-
-api_router = APIRouter()
-
-api_router.include_router(images.router, prefix="/images", tags=["images"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(health.router, prefix="/health", tags=["health"])
